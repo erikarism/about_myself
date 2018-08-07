446d4695a32491567ff5f2cae65d942a5a8ea8cc
erikarism <erikarism@gmail.com>
2018-08-07 15:19:08 -0700

    Project scaddold complete V1

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 'use strict'
 
-alert('it is alive!')
+// Get the user's name
+var userName = prompt('Excuse me, who is you?')
+
+alert('Ugh, ' + userName + ", it's you again. Soooo, since i'm probably more important than you, i'm going to ask you 5 yes or no questions about myself. If you fail, it's to the rancor pits with you")
+
+// Ask them five yes/no questions
+var answerOne = prompt("Is Erik's favorite color fuchsia?").toLowerCase()
+
+if (answerOne === 'n' || answerOne === 'no') {
+  alert('alright, great, 4 more questions!')
+} else {
+  alert('FAIL!!!! To the Rancor pit!')
+}
