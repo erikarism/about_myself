6e0e381a24f522e382108f44726be4491fab0823
erikarism <erikarism@gmail.com>
2018-08-08 19:38:05 -0700

    Added Question 6 and part of 7

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 'use strict'
 
-// Get the user's name
+Get the user's name
 var userName = prompt('Excuse me, who is you?')
 
-alert('Ugh, ' + userName + ', it\'s you again. Soooo, since i\'m probably more important than you, i\'m going to ask you 5 yes or no questions about myself. If you fail, it\'s to the rancor pits with you!')
+alert('Ugh, ' + userName + ', it\'s you again. Soooo, since i\'m obviously a celebrity, i\'m going to ask you 5 yes or no questions about myself. If you fail, you\'ll owe me 5 dollars. If you get it right, then you still owe me 5 dollars.')
 
 // Ask them five yes/no questions
 var answerOne = prompt('Is Erik\'s favorite color fuchsia?').toLowerCase()
@@ -11,42 +11,92 @@ var answerOne = prompt('Is Erik\'s favorite color fuchsia?').toLowerCase()
 if (answerOne === 'n' || answerOne === 'no') {
   alert('Alright, great, 4 more questions!')
 } else {
-  alert('FAIL!!!! To the Rancor pit!')
+  alert('FAILED! Try again, or don\'t... and go do something productive. Don\'t forget about my 5 dollars.')
 }
 console.log(userName + ' was asked if erik\'s favorite color Fuchsia. ' + userName + ' responded ' + answerOne)
 
-var answerTwo = prompt('Was Erik the king of Narnia but then impeached because he accidentally ate the Faun?').toLowerCase()
+var answerTwo = prompt('Has Erik been in international news?').toLowerCase()
 
 if (answerTwo === 'y' || answerTwo === 'yes') {
-  alert('Yeah, that was an embarrising moment for me...')
+  alert('Yeah, unfortunately it was an embarrising moment... it was 6 months of horrible memes.')
 } else {
-  alert('FAIL!!!! To the Rancor pit!')
+  alert('FAILED! Try again, or don\'t... and go do something productive. Don\'t forget about my 5 dollars.')
 }
-console.log(userName + ' was asked if Erik the king of Narnia but then impeached because he accidentally ate the Faun. ' + userName + ' responded ' + answerTwo)
+console.log(userName + ' was asked if Erik was in international news. ' + userName + ' responded ' + answerTwo)
 
-var answerThree = prompt('Can Erik do a triple backflip?').toLowerCase()
+var answerThree = prompt('Can Erik do a double backflip?').toLowerCase()
 
 if (answerThree === 'n' || answerThree === 'no') {
   alert('If you count him landing on his face after the first flip then yes...')
 } else {
-  alert('FAIL!!!! To the Rancor pit!')
+  alert('FAILED! Try again, or don\'t... and go do something productive. Don\'t forget about my 5 dollars.')
 }
-console.log(userName + ' was asked if Erik can do a triple backlfip. ' + userName + ' responded ' + answerThree)
+console.log(userName + ' was asked if Erik can do a double backlfip. ' + userName + ' responded ' + answerThree)
 
-var answerFour = prompt('Does Erik own a ferrari?').toLowerCase()
+var answerFour = prompt('Does Erik a homeowner?').toLowerCase()
 
 if (answerFour === 'n' || answerFour === 'no') {
-  alert('You guess correctly that he is broke... like really broke')
+  alert('You guess correctly, he\'s broke... the closest he is to owning a home is the cardboard box he still haven\'t thrown away yet. I probably should throw that away.')
 } else {
-  alert('FAIL!!!! To the Rancor pit!')
+  alert('FAILED! Try again, or don\'t... and go do something productive. Don\'t forget about my 5 dollars.')
 }
-console.log(userName + ' was asked if Erik owns a Ferrari. ' + userName + ' responded ' + answerFour)
+console.log(userName + ' was asked if Erik is a homeowner. ' + userName + ' responded ' + answerFour)
 
-var answerFive = prompt('Is Erik the greatest person in the world?').toLowerCase()
+var answerFive = prompt('Has Erik gone skydiving?').toLowerCase()
 
 if (answerFive === 'y' || answerFive === 'yes') {
-  alert('You guessed correct! He is probably better than you even though he was impeached from Narnia... doesn\'t own a ferrari.... or can\'t do a triple backflip.')
+  alert('Uh, no. People don\'t fly.)
 } else {
-  alert('FAIL!!!! To the Rancor pit!')
+  alert('FAILED! Try again, or don\'t... and go do something productive. Don\'t forget about my 5 dollars.')
 }
-console.log(userName + ' was asked if Erik is the greatest person in the world. ' + userName + ' responded ' + answerFive)
+console.log(userName + ' was asked if Erik has gone skydiving. ' + userName + ' responded ' + answerFive)
+
+var guesses = 4;
+while(guesses > 0) { //When guesses reach zero, program stops.
+  var answerSix = parseInt(prompt('How old is Erik?')); // converting string to number w/ parseInt()
+  console.log(answerSix);
+  if(answerSix === 27) { // If answer is correct
+    alert('Um, I feel like I turned 21... 7 times.');
+    break; //breaks loop
+  } else {
+    guesses = guesses - 1; //giving the variable guess a new number
+    if (answerSix > 27) {
+      alert('too high, try a little lower.');
+    } else {
+      alert('too low, try a little higher.');
+    }
+  }
+}
+
+var opSystem = ['windows', 'os x'];
+var tF = true;
+var qGuess = 0;
+
+while(qGuess < 6){
+  var answerSeven = prompt('Can you guess what operating systems I know?').toLowerCase();
+
+  console.log(answerSeven);
+  for(var j = 0; j < 2; j++) {
+
+    if (answerSeven === opSystem[j]) { //if answer equals to one answer in array
+    alert('Good job!');
+
+    break;
+  } else {
+    qGuess ++;
+    alert('try again!');
+  }
+}
+}
+
+// var opSystem = ['windows', 'os x'];
+// var tF = true;
+// var qGuess = 6;
+
+// while(qGuess < 7){
+// var answerSeven = prompt('Can you guess what operating systemsI know?').toLowerCase();
+//   console.log(answerSeven);
+//   for(i = 0; i < 2; i++){
+//   if (answerSeven === opSystem[j])
+//   }
+  }
